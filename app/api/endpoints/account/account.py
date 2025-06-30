de54b489bd8c1f7dfba74f43c58f636f88616970
janerson.santos <janerson.santos@cits.br>
2025-06-30 12:50:08 -0300

    Implementing transfer event

--- app/api/endpoints/account/account.py
+++ app/api/endpoints/account/account.py
@@ -37,4 +37,14 @@ def handle_event(event: Event):
 
             return {"origin": {"id": event.origin, "balance": accounts[event.origin]}}
         case 'transfer':
-            return Response(content="OK", media_type="text/plain", status_code=200)
+            if event.origin not in accounts:
+                return Response(content="0", media_type="text/plain", status_code=404)
+
+            accounts[event.origin] -= event.amount
+            current_destination_balance = accounts.get(event.destination, 0)
+            accounts[event.destination] = current_destination_balance + event.amount
+
+            return {
+                "origin": {"id": event.origin, "balance": accounts[event.origin]},
+                "destination": {"id": event.destination, "balance": accounts[event.destination]}
+            }
