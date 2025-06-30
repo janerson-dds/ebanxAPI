f43c8ccfdb46d4673d0cd724046c55c228e7f625
janerson.santos <janerson.santos@cits.br>
2025-06-30 10:13:51 -0300

    Implementing reset and balance endpoints

--- app/api/endpoints/account/account.py
+++ app/api/endpoints/account/account.py
@@ -1,16 +1,21 @@
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Response, HTTPException
 
 router = APIRouter()
+
+accounts = {}
 
 
 @router.post("/reset")
 def reset_state():
+    accounts.clear()
     return Response(content="OK", media_type="text/plain", status_code=200)
 
 
 @router.get("/balance")
-def reset_state():
-    return Response(content="OK", media_type="text/plain", status_code=200)
+def reset_state(account_id: str):
+    if account_id not in accounts:
+        raise HTTPException(status_code=404, detail="Account not found")
+    return accounts[account_id]
 
 
 @router.post("/event")
